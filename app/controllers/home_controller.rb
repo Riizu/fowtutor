25b4128360d9d76ac7627ca7d9fc7a39eccacd58
Ryan Batty <rgbatty@outlook.com>
2018-02-01 16:54:03 -0700

    Add basic home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    redirect_to cards_path
+    @five_most_recent_decks = Decklist.last(5)
+    @five_most_viewed_decks = Decklist.last(5)
+    @five_top_decks = Decklist.last(5)
   end
 end
