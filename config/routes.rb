3bc98c676a33bff90c1ffc00c223b475e8994ee2
Ryan Batty <rgbatty@outlook.com>
2018-02-09 13:22:54 -0700

    Implement filtered search for mini card list

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :comments, module: :users
   end
+
+  namespace :cards do
+    resources :search, only: [:create]
+  end
 end
